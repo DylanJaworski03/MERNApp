46d5f385322a0eb2a9fb1afbf142af4e506d9e1a
DylanJaworski03 <125913213+DylanJaworski03@users.noreply.github.com>
2024-01-28 20:04:18 -0500

    Section 7 Completed
    Utilized HTTP-only cookie
    Registered Users and Generated Tokens
    Finalized User Profile endpoints and checked using postman

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,30 @@
-import express from 'express'; 
-
-import dotenv from 'dotenv'; 
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
-import connectDB from './config/db.js'; 
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js'
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-const port = process.env.PORT || 5000; 
-connectDB(); 
-const app = express(); 
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-app.get('/', (req, res) =>{
-    res.send('API is running...');
-})
+const port = process.env.PORT || 5000;
 
-app.use('/api/products', productRoutes)
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use(notFound); 
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`)); 
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
