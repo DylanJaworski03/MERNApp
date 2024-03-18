955d3bce1e601ee1f68317e29377bb345513291a
DylanJaworski03 <125913213+DylanJaworski03@users.noreply.github.com>
2024-03-18 14:37:50 -0400

    Competed Section 12-14
    Fully Implemented Review Section
    Fully Implemented Search Box
    Fully Implemented Product Carousel and Customized Page Sizes using Paginate
    Prepared for Production
    Utilized Render to push out production

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -69,13 +69,12 @@ const OrderScreen = () => {
     });
   }
 
-  async function onApproveTest() {
-    await payOrder({ orderId, details: { payer: {} } });
-    refetch();
-
-    toast.success('Order is paid');
-  }
-
+  // TESTING ONLY! REMOVE BEFORE PRODUCTION
+  // async function onApproveTest() {
+  //   await payOrder({ orderId, details: { payer: {} } });
+  //   refetch();
+    //   toast.success('Order is paid');
+  // }
   function onError(err) {
     toast.error(err.message);
   }
@@ -215,12 +214,14 @@ const OrderScreen = () => {
                     <Loader />
                   ) : (
                     <div>
-                      <Button
+                      {/* THIS BUTTON IS FOR TESTING! REMOVE BEFORE PRODUCTION! */}
+                      {/* <Button
                         style={{ marginBottom: '10px' }}
                         onClick={onApproveTest}
                       >
                         Test Pay Order
-                      </Button>
+                      
+                      </Button> */}
 
                       <div>
                         <PayPalButtons
